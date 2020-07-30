62b2948db311031a3585344b5b3009eac572b37d
Dani <daniel.avila@electronicid.eu>
2020-07-30 12:18:33 +0200

    Initial Commit

---
+++ Rick&Morty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rick&Morty
+//
+//  Created by Daniel Ávila Domingo on 30/07/2020.
+//  Copyright © 2020 Daniel Ávila Domingo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Rick&MortyTests/Rick_MortyTests.swift
@@ -0,0 +1,34 @@
+//
+//  Rick_MortyTests.swift
+//  Rick&MortyTests
+//
+//  Created by Daniel Ávila Domingo on 30/07/2020.
+//  Copyright © 2020 Daniel Ávila Domingo. All rights reserved.
+//
+
+import XCTest
+@testable import Rick_Morty
+
+class Rick_MortyTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
